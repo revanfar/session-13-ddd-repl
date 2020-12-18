53c5a8c4da93307f243940cfe2e59e1eedcb6fae
revanasw <revan_f_aswariza@telkomsel.co.id>
2020-12-18 19:21:51 +0700

    Change directory name

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var Person = require('./domain/person/person.logic');
-var Age = require('./domain/age/age.logic');
+var Age = require('./domain/personAge/personAge.logic');
 
 var person1 = new Person('Revan', '3451332478230016', '1994-07-25');
+
 if (person1.getWeekType() === 'Weekday' || person1.getWeekType() === 'Weekend') {
-console.log(`${person1.name} lahir pada tipe minggu ${person1.getWeekType()}`)
-} else {
-console.log(`Tanggal lahir ${person1.name} bermasalah`)  
-}
+    console.log(`${person1.name} lahir pada tipe minggu ${person1.getWeekType()}`)
+    } else {
+    console.log(`Tanggal lahir ${person1.name} bermasalah`)  
+    }
+    
 console.log(`Saat ini berumur ${person1.getAge()}`)
 
 var age1 = new Age('Revan', '3451332478230016', '1994-07-25');
